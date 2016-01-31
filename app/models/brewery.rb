0f36a219bb47cf3ed14068b679f1912fd3d0ab5b
Mathias Keus <mathias.keus@phz.fi>
2016-01-31 16:14:15 +0200

    Ex3: Added to_s method to ratings

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,14 @@
 class Brewery < ActiveRecord::Base
 	has_many :beers
+
+	def print_report
+		puts self.name
+		puts "established at year #{year}"
+		puts "number of beers #{beers.count}"
+	end
+
+	def restart
+		self.year = 2016
+		puts "changed year ot #{year}"
+	end
 end
