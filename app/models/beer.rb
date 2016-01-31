97088f64d48cfbe66876558d99f53bee4998d221
Mathias Keus <mathias.keus@phz.fi>
2016-01-31 16:43:08 +0200

    Ex4: Added showing and metods for average rating for beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
   	has_many :ratings
+
+  	def average_rating
+  		total = self.ratings.reduce 0 do |sum, n|
+  			sum + n.score
+		end
+
+  		if self.ratings.count > 0
+  			total.to_f / self.ratings.count
+  		end
+  	end
 end
