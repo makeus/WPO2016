2db22640c01dbeb8202f39ce36a08092e5b9c46d
Mathias Keus <mathias.keus@phz.fi>
2016-02-07 15:40:59 +0200

    Ex2: Login logout links

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
   resource :session, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
