7240b6ca92c19ef5e5258b68bd63d803310c51be
Mathias Keus <mathias.keus@phz.fi>
2016-02-07 16:02:17 +0200

    Ex3: Added dependency from ratings to user, modified user page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,9 +1,9 @@
 class Beer < ActiveRecord::Base
-	include RatingAverage
-	belongs_to :brewery
-  	has_many :ratings, dependent: :destroy
+    include RatingAverage
+    belongs_to :brewery
+    has_many :ratings, dependent: :destroy
 
-  	def to_s
-  		"#{brewery.name}: #{name}"
-	end
+    def to_s
+        "#{brewery.name}: #{name}"
+    end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,17 +1,17 @@
 class Brewery < ActiveRecord::Base
-	include RatingAverage
-	has_many :beers, dependent: :destroy
-	has_many :ratings, through: :beers
+    include RatingAverage
+    has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
 
-	def print_report
-		puts self.name
-		puts "established at year #{year}"
-		puts "number of beers #{beers.count}"
-	end
+    def print_report
+        puts self.name
+        puts "established at year #{year}"
+        puts "number of beers #{beers.count}"
+    end
 
-	def restart
-		self.year = 2016
-		puts "changed year ot #{year}"
-	end
+    def restart
+        self.year = 2016
+        puts "changed year ot #{year}"
+    end
 
 end
