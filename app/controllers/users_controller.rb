5b28c01dcbaeb9850145c7d552a5b603c2852aa0
Mathias Keus <mathias.keus@phz.fi>
2016-02-07 19:34:02 +0200

    Ex14 Modified user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,11 +41,11 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if current_user == @user && @user.update(user_params)
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -72,4 +72,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation)
     end
-end
+  end
