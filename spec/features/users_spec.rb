da7feab51a9a172871a78ff6b9b8373132a20663
Mathias Keus <mathias.keus@phz.fi>
2016-02-14 19:34:54 +0200

    Ex7: Added user rating tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 include Helpers
 
 describe "User" do
-    before :each do
-        FactoryGirl.create :user
-    end
+    let!(:user) { FactoryGirl.create :user }
 
     describe "who has signed up" do
         it "can signin with right credentials" do
@@ -26,5 +24,37 @@ describe "User" do
         fill_in('user_password', with:'Secret55')
         fill_in('user_password_confirmation', with:'Secret55')
         expect{click_button('Create User')}.to change{User.count}.by(1)
+    end
+
+    describe "regarding user ratings" do
+
+        let!(:user2) { FactoryGirl.create :user, username: "ToinenJäbä" }
+
+        it "without users no ratings should be shown on user page" do
+            sign_in(username:"Pekka", password:"wrong")
+            visit user_path(user)
+            expect(page).not_to have_content "Ratings"
+        end
+
+        it "user shouldnt see ratings made by other users on his page" do
+            brewery = FactoryGirl.create :brewery
+            beer = FactoryGirl.create :beer, brewery:brewery
+            FactoryGirl.create :rating, beer:beer, user:user2
+
+            sign_in(username:"Pekka", password:"wrong")
+            visit user_path(user)
+            expect(page).not_to have_content "Ratings"
+        end
+
+        it "user should see his own ratings on his page" do
+            brewery = FactoryGirl.create :brewery
+            beer = FactoryGirl.create :beer, brewery:brewery
+            FactoryGirl.create :rating, score:5, beer:beer, user:user
+            FactoryGirl.create :rating, score:10, beer:beer, user:user
+
+            sign_in(username:"Pekka", password:"wrong")
+            visit user_path(user)
+            expect(page).to have_content "Has made 2 ratings"
+        end
     end
 end
