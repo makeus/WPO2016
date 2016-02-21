af3e3580800e40552f06839483834175dfc262e6
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 22:05:49 +0200

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,15 @@ b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-b3.beers.create name:"Hefeweizen", style:"Weizen"
-b3.beers.create name:"Helles", style:"Lager"
+style1 = Style.create name:"Lager", description: "Lager (German: storeroom or warehouse) is a type of beer that is conditioned at low temperatures, normally in cold storage at the brewery, before being delivered to the consumer. It may be pale, golden, amber, or dark."
+style2 = Style.create name:"Pale Ale", description: "Pale ale is a beer made by warm fermentation using predominantly pale malt."
+style3 = Style.create name:"Porter", description: "Porter is a dark style of beer developed in London from well-hopped beers made from brown malt. The name was first recorded in the 18th century, and is thought to come from its popularity with street and river porters"
+style4 = Style.create name:"Weizen"
+
+b1.beers.create name:"Iso 3", style:style1
+b1.beers.create name:"Karhu", style:style1
+b1.beers.create name:"Tuplahumala", style:style1
+b2.beers.create name:"Huvila Pale Ale", style:style2
+b2.beers.create name:"X Porter", style:style3
+b3.beers.create name:"Hefeweizen", style:style4
+b3.beers.create name:"Helles", style:style1
