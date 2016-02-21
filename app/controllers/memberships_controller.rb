16f1a92d8bd69a442c94cf39b091916ab59bcb6b
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 20:36:49 +0200

    Ex9: Added removal of membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -58,7 +58,7 @@ class MembershipsController < ApplicationController
   def destroy
     @membership.destroy
     respond_to do |format|
-      format.html { redirect_to memberships_url, notice: 'Membership was successfully destroyed.' }
+      format.html { redirect_to @membership.beer_club, flash: {success: "membership in #{@membership.beer_club.name} ended." }}
       format.json { head :no_content }
     end
   end
