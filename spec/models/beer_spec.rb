7eafa547822a83c43c2b714e01b6cdb27f166c26
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 21:51:00 +0200

    Ex14: Fixed tests

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Beer, type: :model do
+
+    let!(:style) { FactoryGirl.create :style }
+
     it "with proper name and style is saved" do
-        beer = Beer.new name:"Testiolut", style:"Lager"
+        beer = Beer.new name:"Testiolut", style:style
         expect(beer).to be_valid
         expect(beer.name).to eq("Testiolut")
     end
 
     describe "it is not saved" do
         it "without name" do
-            beer = Beer.new style:"Lager"
+            beer = Beer.new style:style
             expect(beer).not_to be_valid
             expect(Beer.count).to eq(0)
         end
