ba7e93d2efdd679eb337914f78d575decff249a8
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 19:18:18 +0200

    Ex7: Membership creation on beer club page
    Also ratings on beer page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,11 +6,9 @@ class PlacesController < ApplicationController
     if session[:latestcity].nil?
         redirect_to places_path, notice: "No place saved for id #{oarams[:id]}"
     end
-
-    @place = BeermappingApi.places_in(session[:latestcity]).select{|place|place.id = params[:id]}.first
-    
+    @place = BeermappingApi.places_in(session[:latestcity]).select{|place|place.id == params[:id]}.first
     if @place.nil?
-        redirect_to places_path, notice: "No place saved for id #{oarams[:id]}"
+        redirect_to places_path, notice: "No place for id #{params[:id]} found"
     end
   end
 
