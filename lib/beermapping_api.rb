83c73b34b6691b7a04751b00a6a04465f77ac44d
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 16:49:24 +0200

    Ex4: Added caching to beermapping api

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,4 +1,12 @@
 class BeermappingApi
+
+  def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 1.week) { fetch_places_in(city) }
+  end
+
+  private 
+
   def self.places_in(city)
     url = 'http://stark-oasis-9187.herokuapp.com/api/'
 
