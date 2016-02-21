dcf34b96441d751a4559caef316eacf50255e039
Mathias Keus <mathias.keus@phz.fi>
2016-02-21 21:51:00 +0200

    Ex14: Fixed tests

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -78,6 +78,6 @@ class BeersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def beer_params
-      params.require(:beer).permit(:name, :style, :brewery_id)
+      params.require(:beer).permit(:name, :style_id, :brewery_id)
     end
   end
